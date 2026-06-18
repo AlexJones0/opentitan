f376f4c1e02fe49b556834847698678c34a28190
Alex Jones <alex.jones@lowrisc.org>
2026-06-19 00:25:07 +0100

    [opentitantool] Add initial FPGA bkdr_loader entry & discovery logic
    This commit implements the initial logic for integrating the FPGA
    backdoor loader into OpenTitanLib & OpenTitanTool. It provides
    interfaces for connecting to the bkdr_loader TAP via OpenOCD, entering
    the backdoor loader (applying the TAP strapping and resetting), and for
    enumerating/discovering the target information from the CSRs.
    
    A new `opentitantool bkdr` command is implemented for using this
    functionality, with 3 subcommands - `enter` to enter the backdoor
    loader, `exit` to finish using it and take OT out of reset, and `info`
    to list info about the available targets (or a specific target). As part
    of this, there is some logic for looking up a target by its unique ID.
    
    Note that we return handles for each of the different targets which
    simply wrap the `BackdoorTargetInfo` - these will be extended in future
    commits to allow operations on the various targets.
    
    Signed-off-by: Alex Jones <alex.jones@lowrisc.org>

---
+++ sw/host/opentitantool/src/command/bkdr.rs
@@ -0,0 +1,105 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+use anyhow::{Context, Result};
+use clap::{Args, Subcommand};
+use std::any::Any;
+use std::convert::From;
+
+use opentitanlib::app::TransportWrapper;
+use opentitanlib::app::command::CommandDispatch;
+use opentitanlib::io::fpga_bkdr::{BackdoorParams, enter_backdoor_loader};
+
+/// Commands for interacting with the backdoor FPGA loader.
+#[derive(Debug, Subcommand, CommandDispatch)]
+pub enum InternalBkdrCommand {
+    /// Enter the backdoor loader - this *requires* resetting the device.
+    Enter(BkdrEnter),
+    /// Exit the backdoor loader, finishing and de-asserting reset. Irreversible until next reset.
+    Exit(BkdrExit),
+    /// Display information about the available backdoor targets
+    Info(BkdrInfo),
+}
+
+#[derive(Debug, Args)]
+pub struct BkdrEnter {}
+
+impl CommandDispatch for BkdrEnter {
+    fn run(
+        &self,
+        _context: &dyn Any,
+        transport: &TransportWrapper,
+    ) -> Result<Option<Box<dyn erased_serde::Serialize>>> {
+        enter_backdoor_loader(transport)?;
+
+        Ok(None)
+    }
+}
+
+#[derive(Debug, Args)]
+pub struct BkdrExit {}
+
+impl CommandDispatch for BkdrExit {
+    fn run(
+        &self,
+        context: &dyn Any,
+        transport: &TransportWrapper,
+    ) -> Result<Option<Box<dyn erased_serde::Serialize>>> {
+        let context = context.downcast_ref::<BkdrCommand>().unwrap();
+        let bkdr = context.params.create(transport)?;
+        let bkdr = bkdr.connect(false)?;
+        bkdr.set_done()?;
+
+        Ok(None)
+    }
+}
+
+#[derive(Debug, Args)]
+pub struct BkdrInfo {
+    /// Optional target to query. If not specified, returns info for all targets.
+    pub target: Option<String>,
+}
+
+impl CommandDispatch for BkdrInfo {
+    fn run(
+        &self,
+        context: &dyn Any,
+        transport: &TransportWrapper,
+    ) -> Result<Option<Box<dyn erased_serde::Serialize>>> {
+        let context = context.downcast_ref::<BkdrCommand>().unwrap();
+        let bkdr = context.params.create(transport)?;
+        let mut bkdr = bkdr.connect(true)?;
+
+        let info: Box<dyn erased_serde::Serialize> = match &self.target {
+            Some(id_str) => {
+                let target = bkdr
+                    .target_by_id_str(id_str)?
+                    .context(format!("FPGA target '{id_str}' not found"))?;
+                Box::new(target.info)
+            }
+            None => Box::new(bkdr.targets().to_vec()),
+        };
+
+        Ok(Some(info))
+    }
+}
+
+#[derive(Debug, Args)]
+pub struct BkdrCommand {
+    #[command(flatten)]
+    params: BackdoorParams,
+
+    #[command(subcommand)]
+    command: InternalBkdrCommand,
+}
+
+impl CommandDispatch for BkdrCommand {
+    fn run(
+        &self,
+        _context: &dyn Any,
+        transport: &TransportWrapper,
+    ) -> Result<Option<Box<dyn erased_serde::Serialize>>> {
+        self.command.run(self, transport)
+    }
+}
